1c10bec9a1b7231687e08244f372a701065f1bad
Théo <theo@Theos-MacBook-Air.local>
2024-10-16 18:02:23 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let UserInput = prompt("Entre ton prénom", "Ton prénom");
+function sayHello(firstname, hour) {
+    const message = "Bonjour " + UserInput + "!";
+     if (hour >= 18) {
+        document.querySelector('h1').innerText = "Bonsoir " + UserInput + "!";
+     } else {
+        document.querySelector('h1').innerText = message;
+     }
+    } 
+sayHello("Beyonce", 15);
